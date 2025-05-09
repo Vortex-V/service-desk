800df9fdfe0ba1cf679414d12ced4565c9415472
Vitaliy <vitalik124124@mail.ru>
2025-05-09 19:34:29 +0300

    fix mobile tableview

--- resources/views/components/bs/table/table.blade.php
+++ resources/views/components/bs/table/table.blade.php
@@ -1,3 +1,5 @@
-<table class="table table-striped-columns table-hover">
-    {{ $slot }}
-</table>
+<div class="overflow-x-auto w-100">
+    <table class="table table-striped-columns table-hover">
+        {{ $slot }}
+    </table>
+</div>
